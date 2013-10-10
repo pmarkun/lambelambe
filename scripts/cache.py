dd5d8f2b5ed00298e84690426a2af6b185b9b841
Pedro Markun <pedro@markun.com.br>
2013-10-10 17:21:08 -0300

    cache generator with improvements

--- scripts/cache.py
+++ scripts/cache.py
@@ -1,17 +1,33 @@
 from urllib2 import quote
-import subprocess, json
+import threading, subprocess, json
 from time import sleep
 from os import path
+from unidecode import unidecode
+
+class RunCmd(threading.Thread):
+    def __init__(self, cmd, timeout):
+        threading.Thread.__init__(self)
+        self.cmd = cmd
+        self.timeout = timeout
+
+    def run(self):
+        self.p = subprocess.Popen(self.cmd, close_fds=True)
+        self.p.wait()
+
+    def Run(self):
+        self.start()
+        self.join(self.timeout)
+
+        if self.is_alive():
+            self.p.terminate()
+            self.join()
 
 lambes = json.loads(open('../transparencia.json', 'r').read())
 estados = ["AC","AL","AM","AP","BA","CE","DF","ES","EX","GO","MA","MG","MS","MT","PA","PB","PE","PI","PR","RJ","RN","RO","RR","RS","SC","SE","SP","TO"]
 def generate_image(c):
     if not path.isfile('../static/raw/' + c['file'] + '-hi.png'):
         url = c['url']
-        subprocess.call(['phantomjs', 'rasterize.js', url, '.lambe', '../static/raw/' + c['file'] + '-hi.png'])
-        sleep(3)
-        #subprocess.call(['phantomjs','rasterize.js', url, '.lambe', '../static/raw/' + c['file'] + '.pdf'])
-        sleep(8)
+        RunCmd(['phantomjs', 'rasterize.js', url, '.lambe', '../static/raw/' + c['file'] + '-hi.png'], 10).run()
     print 'Zzz...'
 
 cache_urls = []
@@ -21,11 +37,11 @@ for orgao_a in lambes:
             if estado_a in estados and estado_a in lambes[orgao_b]:
                  cache_urls += [{
                     'url' : 'http://127.0.0.1:5000/l/' + quote('/'.join([orgao_a,estado_a,orgao_b,estado_a]).encode('utf-8')),
-                    'file' : '-'.join([orgao_a,estado_a,orgao_b,estado_a]).encode('utf-8')
+                    'file' : unidecode('-'.join([orgao_a,estado_a,orgao_b,estado_a]))
                 }]
                  cache_urls += [{
                     'url' : 'http://127.0.0.1:5000/l/' + quote('/'.join([orgao_b,estado_a,orgao_a,estado_a]).encode('utf-8')),
-                    'file' : '-'.join([orgao_b,estado_a,orgao_a,estado_a])
+                    'file' : unidecode('-'.join([orgao_b,estado_a,orgao_a,estado_a]))
                 }]
 
 for c in cache_urls:
